da0f9156d4139010992aebcd3d8a6247b9897c51
saifuddin <saif.afridi62@gmail.com>
2021-11-11 16:36:20 +0500

    more fuctins added

--- getbyid.php
+++ getbyid.php
@@ -14,30 +14,25 @@ header('Access-Control-Max-Age: 86400');
 
 
 $conn = new mysqli("localhost","root","","weberp");
-$sql="SELECT * FROM invoicea";
-$result = mysqli_query($conn,$sql) or die("Connection Faild");;
-$output = mysqli_fetch_all($result,MYSQLI_ASSOC);
-echo json_encode($output);
+//$result = mysqli_query($conn,$sql) or die("Connection Faild");;
 
 
 
-$myPost = file_get_contents("php://input");
-if($myPost >0 )
-{
-    $output2 = json_decode($myPost);
-    $addQuery = "INSERT INTO `invoicea` (`Id`, `InvoiceId`, `Date`, `CustomerName`, `FatherName`, `MobileNumber`, `LisenceNumber`, `Address`, `Note`) 
-                  VALUES (NULL,'25', '{$output2->date}', '{$output2->customerName}', '{$output2->fatherName}', '{$output2->mobileNumber}', '{$output2->lisenceNumber}', '{$output2->address}', '{$output2->note}')";  
-    $result = mysqli_query($conn,$addQuery) or die("Connection Faild");
- }
 
 
-    //$deleteId = $_GET['id'];
-  
 
+
+
+    $updateId = $_GET['id'];  
+    //echo $deleteId;
    
-    //$deleteQuery =  "DELETE FROM `invoicea` WHERE `Id`='{$deleteId}'";
-    //$result = mysqli_query($conn,$deleteQuery) or die("Connection Faild");
+    $deleteQuery =  "SELECT * FROM `invoicea` WHERE `Id`='{$updateId}'";
 
+  
+    $result = mysqli_query($conn,$deleteQuery) or die("Connection Faild");
+    $output = mysqli_fetch_all($result,MYSQLI_ASSOC);
+    echo json_encode($output);
+   
 
 // file_put_contents(); any file if you want to chane
 // file_get_contents(); any file if you want to read
